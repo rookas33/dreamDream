87a634f6559f658b2f9e03a617839b8bfb992ebd
jeong chang min <ckdalsla1994@gmail.com>
2023-02-02 09:44:24 +0900

    사라진 index.js 추가, ejs파일, app.js 조금 수정

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const authMiddleware = require("./middlewares/auth-middleware");
-const { Server } = require("http");
-const socketIo = require("socket.io");
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const { Op } = require("sequelize");
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const app = express();
-const http = Server(app);
-const io = socketIo(http);
 
+// socket.io
+const http = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+// 미들웨어
 app.set("view engine", "ejs");
-app.set("views", "./views");
-
-const cookieParser = require("cookie-parser");
+app.use('/public', express.static('public'))
 app.use(cookieParser());
+app.use(express.json())
 
-// 매니저 api
-app.use("/users/manager");
+// 메인 페이지
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
-app.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
     console.log(`${process.env.PORT} 포트가 열렸습니다`)
 });
-e.exports = app;
